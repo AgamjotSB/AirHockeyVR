b9a7ddbd3d6a9ecf2df8c1d78ca6e4cd5cc0fc7d
Ishan Bhargava <ishan1939.be21@chitkara.edu.in>
2024-11-22 23:05:30 +0530

    Add VR Interaction

--- Assets/Scripts/PaddleVR.cs
+++ Assets/Scripts/PaddleVR.cs
@@ -1,10 +1,10 @@
-using TMPro;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class PaddleVR : MonoBehaviour
 {
 
-	[SerializeField] Transform target;
+	Transform target;
 
 	float arenaExtents = 10f, extents = 2f;
 
@@ -12,22 +12,36 @@ public class PaddleVR : MonoBehaviour
     }
 
     void Update() {
-        Move();
-    }
+		if (target != null) {
+			Move();
+		}
+	}
 
 	public void StartNewGame () {
 	}
 
-    public void Move ()
-	{
+    public void Move () {
 		Vector3 p = transform.position;
-		p.x = target.position.x;
+		Vector3 intersect = new Plane(transform.up, p).RaycastPoint(new Ray(target.position, target.forward));
+		p.x = intersect.x;
 		float limit = arenaExtents - extents;
 		p.x = Mathf.Clamp(p.x, -limit, limit);
 		transform.position = p;
 		p = transform.localPosition;
 		p.x = Mathf.Clamp(p.x, -limit, limit);
 		transform.localPosition = p;
+	}
+
+	public void startHover(SelectEnterEventArgs args) {
+		if (target == null) {
+			target = args.interactorObject.transform;
+		}
+	}
+
+	public void endHover(SelectExitEventArgs args) {
+		if (target == args.interactorObject.transform) {
+			target = null;
+		}
 	}
 
 }
